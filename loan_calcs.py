41bd6c86bfa06427395788a77b20502620eb6f54
mdebayu <mdebayu@gmail.com>
2017-07-09 12:26:39 +1000

    Added primary variables

--- loan_calcs.py
+++ loan_calcs.py
@@ -1,5 +1,29 @@
 def main():
-	print "something";
+	principal = 26500;
+	print ("Principal = " + str(principal) + "\n");
+	monthly_repayment = 444.55;
+	print ("Monthly Repayment= " + str(monthly_repayment) +"\n");
+
+	yrly_interest_rate = 5.70;
+	years = 5;
+
+	terms  = 12 * years;
+	daily_rate_multiplier = (100+yrly_interest_rate/365)/100;
+
+
+
+
+def calculate_interest(balance, termly_interest_rate):
+	return balance * termly_interest_rate;
+
+def make_payment(balance,payment):
+	return balance - payment;
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
