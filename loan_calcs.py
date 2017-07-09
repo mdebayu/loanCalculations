78616435381afd7b155236f539444b29fbac33c6
mdebayu <mdebayu@gmail.com>
2017-07-10 00:18:44 +1000

    Added calculate Monthly Payments function

--- loan_calcs.py
+++ loan_calcs.py
@@ -4,14 +4,15 @@ def main():
 	monthly_repayment = 444.55;
 	print ("Monthly Repayment= " + str(monthly_repayment) +"\n");
 
-	yrly_interest_rate = 5.70;
+	_yrly_interest_rate = 5.70;
 	years = 5;
 
 	terms  = 12 * years;
-	daily_rate_multiplier = (100+yrly_interest_rate/365)/100;
+	daily_rate_multiplier = get_daily_rate (_yrly_interest_rate)
 
 
-
+def get_daily_rate(_rate):
+    return (100+_rate/12)/100;
 
 def calculate_interest(balance, termly_interest_rate):
 	return balance * termly_interest_rate;
@@ -19,7 +20,9 @@ def calculate_interest(balance, termly_interest_rate):
 def make_payment(balance,payment):
 	return balance - payment;
 
-
+def calculate_monthly_repayment(balance, rate, terms, balloon_payment):
+    assert(rate>1);
+    return((balance*(rate**terms)-balloon_payment) * (rate-1 ))/(rate**(terms-1)-1)
 
 
 
@@ -27,4 +30,8 @@ def make_payment(balance,payment):
 
 
 if __name__ == "__main__":
-	main();
+    p=calculate_monthly_repayment(26500,get_daily_rate(5.7),60,5300)
+    print p;
+
+
+#main();
